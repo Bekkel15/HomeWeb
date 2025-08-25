c2450ca2a715245073b6bbe6d362c664dc56ff86
bekkel15 <rexup15@gmail.com>
2025-08-25 13:32:26 +0300

    HomeWeb

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import './js/modal-window';
+import './js/work-tgthr-footer';
+import './js/reviews.js';
+import { faqProjects } from './js/faq.js';
+import './js/projects-render';
+import {} from 'module';
+
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+// Header and Hero
+
+import {
+  toggleDarkMode,
+  setupMobileMenuListeners,
+} from './js/header-logo.js';
+
+document
+  .getElementById('toggle-background')
+  .addEventListener('click', toggleDarkMode);
+
+setupMobileMenuListeners();
+document.addEventListener('DOMContentLoaded', faqProjects);
